a09291648a65682f537e860d922fc27ed5f432c0
Jason Molloy <jmolloy619@gmail.com>
2018-10-29 14:38:00 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+var jason = {
+  firstName: "Jason",
+  middleName: "Matthew",
+  lastName: "Molloy",
+  DOB: "June 13th, 1979",
+  hair: "brown",
+  eyes: "blue eyes",
+  wife: "Kylee",
+  son: "Asher Molloy",
+  father: "Art Molloy",
+  mother: "Lisa Molloy",
+  brother: "Mike Molloy",
+  sister: "Tiffany Molloy",
+  halfSister: "Samantha Hauri",
+  stepFather: "Craig Hauri",
+  stepMother: "Denise DiChairo",
+  exWife: "Kristina Molloy",
+  birthplace: "El Cajon, California",
+  elementarySchool: "Flying Hills Elementary",
+  juniorHighSchool: "Cajon Valley Jr. High",
+  highSchoolOne: "Grossmont High School",
+  highSchoolTwo: "University City High School",
+  favoriteCity: "Boise, Idaho",
+  dadsCity: "San Diego, California"
+};
+console.log(`My full name is ${jason.firstName} ${jason.middleName} ${jason.lastName} and I was born in ${jason.birthplace} on ${jason.DOB}, to ${jason.father} and ${jason.mother}. 
+I was their first child and over the next three years our family grew to include my sister, ${jason.sister}, and brother, ${jason.brother}. 
+Though it was a little lighter at birth, my hair is naturally ${jason.hair}, and I have ${jason.eyes}.
+As a child I went to ${jason.elementarySchool} and, even though my parents eventually divorced, I had a pretty normal childhood.
+While I was attending ${jason.juniorHighSchool}, ${jason.father} got remarried to, ${jason.stepMother}.
+I started High School in ${jason.birthplace}, at ${jason.highSchoolOne}, but during my sophomore year, my mom remarried, ${jason.stepFather}, and I moved in with my Dad.
+Now in ${jason.dadsCity}, I began attending ${jason.highSchoolTwo}. Meanwhile, my mom and ${jason.stepFather} had a child, ${jason.halfSister}.
+After high school, I travelled a little and ended up moving to ${jason.favoriteCity} where I met ${jason.exWife}. We had a son together, ${jason.son}, but the marriage did not last.
+I moved back to ${jason.dadsCity} and met ${jason.wife}. To make a long story short, I married ${jason.wife} and after a few years of planning and preparing, we have ben able to move back to ${jason.favoriteCity} in order to be closer to ${jason.son}.`)
